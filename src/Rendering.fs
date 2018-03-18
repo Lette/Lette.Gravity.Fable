f893cfe40eaa3f9eaabf97b239a9acf23f530da7
Christoffer Lette <christoffer.lette@gmail.com>
2018-03-18 15:37:39 +0100

    Draws objects with 3D perspective.

--- src/Rendering.fs
+++ src/Rendering.fs
@@ -28,17 +28,17 @@ module Rendering =
     let private ctx = canvas.getContext_2d ()
     ctx.translate (hw, hh)
 
-    let transformFactor depth =
+    let private transformFactor depth =
         ViewingDistance / (ViewingDistance + depth)
 
-    let transformToScreen point =
+    let private transformToScreen point =
         let t = transformFactor point.z
         { x = point.x * t; y = point.y * t; z = 0. }
 
-    let p1 = { x = -hw + 0.5; y = -hh + 0.5; z = 0. }
-    let p1' = transformToScreen { p1 with z = Depth }
-    let p2 = { x = hw - 0.5; y = hh - 0.5; z = 0. }
-    let p2' = transformToScreen { p2 with z = Depth }
+    let private p1 = { x = -hw + 0.5; y = -hh + 0.5; z = 0. }
+    let private p1' = transformToScreen { p1 with z = Depth }
+    let private p2 = { x = hw - 0.5; y = hh - 0.5; z = 0. }
+    let private p2' = transformToScreen { p2 with z = Depth }
 
     let private ellipse x y rx ry startAngle endAngle =
         ctx.save ()
@@ -49,22 +49,22 @@ module Rendering =
         ctx.restore ()
         ctx.stroke ()
 
+    let private drawGreatCircle x y rx ry startAngle middleAngle endAngle =
+        ctx.save ()
+
+        ctx.fillStyle <- transparent
+
+        ctx.strokeStyle <- markerColorB
+        ctx.setLineDash (dashStyle)
+        ellipse x y rx ry middleAngle endAngle
+
+        ctx.strokeStyle <- markerColorF
+        ctx.setLineDash (noDash)
+        ellipse x y rx ry startAngle middleAngle
+
+        ctx.restore ()
+
     let private drawBody body =
-
-        let drawGreatCircle x y rx ry startAngle middleAngle endAngle =
-            ctx.save ()
-
-            ctx.fillStyle <- transparent
-
-            ctx.strokeStyle <- markerColorB
-            ctx.setLineDash (dashStyle)
-            ellipse x y rx ry middleAngle endAngle
-
-            ctx.strokeStyle <- markerColorF
-            ctx.setLineDash (noDash)
-            ellipse x y rx ry startAngle middleAngle
-
-            ctx.restore ()
 
         let drawEquator x y rx ry =
             drawGreatCircle x y rx ry 0. Math.PI (2. * Math.PI)
@@ -72,43 +72,47 @@ module Rendering =
         let drawMeridian x y rx ry =
             drawGreatCircle x y rx ry (-Math.PI / 2.) (Math.PI / 2.) (3. * Math.PI / 2.)
 
-        let drawBody x y r =
+        let createPlanetColor z : U3<string, Browser.CanvasGradient, Browser.CanvasPattern> =
+            !^ (sprintf "rgba(255,100,0,%f" (0.1 + 0.3 * (Depth - z) / Depth))
+
+        let drawFace x y z r =
             ctx.beginPath ()
-            ctx.fillStyle <- planetColor
+            ctx.fillStyle <- (createPlanetColor z)
             ctx.arc (x, y, r, 0., 2. * Math.PI, false)
             ctx.fill ()
 
-        let viewingAngle d dMax =
-            let h = dMax / 2.
-            let maxViewingAngle = Math.PI / 9.
-            maxViewingAngle * (d - h) / h
+        let viewingAngle offset depth =
+            offset / (ViewingDistance + depth) |> atan
 
-        let drawLines x y r =
-            let verticalViewingAngle = viewingAngle y Height
-            drawEquator x y r (r * verticalViewingAngle)
+        let drawLines x y z r =
+            let ry = r * sin (viewingAngle y z)
+            drawEquator x y r ry
 
-            let horizontalViewingAngle = viewingAngle x Width
-            drawMeridian x y (r * horizontalViewingAngle) r
-
-        let draw x y =
-            let r = radius body
-            drawBody x y r
-            drawLines x y (r - 1.)
+            let rx = r * sin (viewingAngle x z)
+            drawMeridian x y rx r
 
         let p = { body.Position with x = body.Position.x - hw; y = body.Position.y - hh }
-        draw p.x p.y
+        let c = transformToScreen p
+
+        let r = radius body
+        let pr1 = transformToScreen { body.Position with x = body.Position.x - r / 2. }
+        let pr2 = transformToScreen { body.Position with x = body.Position.x + r / 2. }
+        let transformedRadius = pr2.x - pr1.x
+
+        drawFace c.x c.y body.Position.z transformedRadius
+        drawLines c.x c.y body.Position.z (transformedRadius - 1.)
 
     let private drawBodies bodies =
         bodies |> List.iter drawBody
 
-    let clearCanvas () =
+    let private clearCanvas () =
         ctx.save ()
         ctx.setTransform (1., 0., 0., 1., 0., 0.)
         ctx.fillStyle <- black
         ctx.fillRect (0., 0., canvas.width, canvas.height)
         ctx.restore ()
 
-    let drawBoundingBox () =
+    let private drawBoundingBox () =
         ctx.save ()
         ctx.strokeStyle <- white
 
